9aeb48c60434c3a5c7d77264fb97cc7e043dd477
22bipp22 <tammibippert@gmail.com>
2020-07-23 12:30:12 -0500

    finished pybank and pypoll challenges, results files included

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,3 +1,4 @@
+#PyBank Homework
 import os
 import csv
 
@@ -5,11 +6,16 @@ csvpath = os.path.join("Resources","budget_data.csv")
 
 budget_date = []
 profit_loss = []
-budget_total = 0
+great_inc_date = str
 greatest_increase = 0
+great_dec_date = str 
 greatest_decrease = 0
-change_amount = []
+profit_change_total = 0
+budget_total = 0
+profit_change = 0
 
+#initialize "previous row" with the first profit/loss value
+first_row = "yes"
 
 
 with open(csvpath) as csvfile:
@@ -24,25 +30,60 @@ with open(csvpath) as csvfile:
         budget_date.append(row[0])
         profit_loss.append(row[1])
 
+        #running total of all of the profit/losses
         budget_total = budget_total + int(row[1])
 
+        #current row profit/loss amount
         budget_amount = int(row[1])
-
-        if budget_amount > greatest_increase:
-            greatest_increase = budget_amount
-
-        if budget_amount < greatest_decrease:
-            greatest_decrease = budget_amount
-
-    
-    ##???? need to loop through profit_loss and subtract previous from current, then add them all and get the average ????
-    for row in profit_loss:
         
-    #calculate total months 
-    total_months = len(budget_date) 
-    print(profit_loss[0])
+        #If this is the first row, save the profit/loss amount to the previous row field
+        if first_row == "yes":
+            
+            previous_row = budget_amount
+            first_row = "no"
+
+        #Subtract the previous profit/loss value from the current p/l value to get the profit change amount and add it to the total change
+        else:
+
+            profit_change = int(budget_amount) - int(previous_row)
+            profit_change_total += profit_change
+                
+            previous_row = budget_amount
+
+        #Looking for the date with the greatest p/l increase
+        if profit_change > greatest_increase:
+            greatest_increase = profit_change
+            great_inc_date = row[0]
+
+        #Looking for the date with the greatest p/l decrease
+        if profit_change < greatest_decrease:
+            greatest_decrease = profit_change
+            great_dec_date = row[0]
+   
     
-    print(total_months)
-    print(budget_total)
-    print(greatest_increase)
-    print(greatest_decrease)
+    #final calculations for display    
+    total_months = len(budget_date)
+    average_change = round(profit_change_total/(total_months - 1),2)
+    
+#Display results in terminal
+print("Financial Analysis")
+print("-------------------------------")
+print(f"Total Months: {total_months}")   
+print(f"Total: ${budget_total}")
+print(f"Average Change: ${average_change}")
+print(f"Greatest Increase in Profits: {great_inc_date} (${greatest_increase})")
+print(f"Greatest Decrease in Profits: {great_dec_date} (${greatest_decrease})")
+        
+#Print results to a text file
+output_file = os.path.join("Analysis","Financial_Summary.txt")
+
+with open(output_file, 'w') as textfile:
+    textfile.writelines(f'Financial Analysis \n-------------------------- \nTotal Months: {total_months} \nTotal: ${budget_total} \nAverage Change: ${average_change} \nGreatest Increase in Profits: {great_inc_date} (${greatest_increase}) \nGreatest Decrease in Profits: {great_dec_date} (${greatest_decrease})') 
+   
+   
+
+    
+    
+
+    
+    
---
+++ PyPoll/main.py
@@ -0,0 +1,80 @@
+#PyPoll Homework
+import os
+import csv
+import zipfile
+
+csvpath = os.path.join("Resources","election_data.csv")
+
+candidate_file = []
+poll_results = [0,0,0,0,0,0,0,0,0,0,0]
+vote_percent = [0,0,0,0,0,0,0,0,0]
+election_results = []
+total_votes = 0
+highest_votes = 0
+
+
+
+with open(csvpath) as csvfile:
+    polldata = csv.reader(csvfile, delimiter = ",")
+    
+    csv_header = next(polldata)
+
+    for row in polldata:
+        
+        candidate_file.append(row[2])
+
+#Remove duplicates of candidates fron candidate file by creating a dictionary key for the list.
+candidate_list = list(dict.fromkeys(candidate_file))
+
+with open(csvpath) as csvfile:
+    polldata = csv.reader(csvfile, delimiter = ",")
+    
+    csv_header = next(polldata)
+  
+    for row in polldata:
+        #Return the position of candidate voted for from the candidate_list then add 1 to the votes for that candidate in the poll results.  Also add 1 to the grand total number of votes.
+        for i in [i for i, candidate in enumerate(candidate_list) if candidate == row[2]]:
+                    
+            poll_results[i] += 1
+            total_votes += 1
+               
+#Final print display to terminal 
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {total_votes}")
+print("-------------------------")
+#Calculate percentages of votes and determine the winner and print the candidate list
+for candidate_index in range(len(candidate_list)):
+    
+    vote_percent[candidate_index] = round((poll_results[candidate_index]/total_votes) * 100,3)
+    print(f"{candidate_list[candidate_index]}: {vote_percent[candidate_index]}% ({poll_results[candidate_index]})")
+
+    if poll_results[candidate_index] > highest_votes:
+        highest_votes = poll_results[candidate_index]
+        winner = candidate_list[candidate_index]
+
+print("-------------------------")
+print(f"Winner: {winner}")
+print("-------------------------")
+
+#Print results to a text file
+output_file = os.path.join("Analysis","Poll_Results.txt")
+
+with open(output_file, 'w') as textfile:
+    
+    textfile.writelines(f'Election Results \n------------------------- \nTotal Votes: {total_votes} \n-------------------------') 
+    
+    #loop through candidate lists and print to file
+    for candidate_index in range(len(candidate_list)):
+        textfile.writelines(f"\n{candidate_list[candidate_index]}: {vote_percent[candidate_index]}% ({poll_results[candidate_index]})")
+    
+    textfile.writelines(f"\n------------------------- \nWinner: {winner} \n-------------------------")
+   
+   
+
+
+
+        
+
+
+        
